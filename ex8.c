a95c09c3e5c620a4eb6cf10dbfe074bf616286ff
Elder <elderzxvm99@gmail.com>
2018-08-27 20:12:45 -0300

    exercicio 8

--- ex8.c
+++ ex8.c
@@ -2,6 +2,11 @@
 
 int main()
 {
-		
-
+	int nConta;
+	float saldo, credito, debito;
+	printf("Digite: \n o numero da conta \n o seu saldo \n credito \n debito \n");
+	scanf("%d %f %f %f", &nConta, &saldo, &credito, &debito);
+	printf("Conta: %d \n saldo: %.2f \n credito: %.2f \n debito: %.2f \n", nConta, saldo, credito, debito);	
+	saldo = saldo - debito + credito;
+	printf("Saldo Atual: %.2f \n", saldo);
 }
