8a130fb1d52aa0a7721619984fde1caf6fd18073
Elder <elderzxvm99@gmail.com>
2018-08-28 09:23:00 -0300

    final fix ex11

--- ex11.c
+++ ex11.c
@@ -2,24 +2,14 @@
 
 int maior(int a, int b)
 {
-	if(a> b)
-	{
-		return a;
-	}else{
-
-		return b;
-	}	
+	if(a> b){return a;}
+	else	{return b;}	
 
 }
 int menor(int a, int b)
 {
-	if(a<b)
-	{
-		return a;
-	 }else{
-		return b;
-	}
-
+	if(a<b){return a;}
+	else{return b;}
 }
 
 
@@ -27,7 +17,6 @@ int main()
 {
 	int homem[2];
 	int mulher[2];
-	int novo, nova, velho, velha;	
 	float results[2];
 
 	printf("Digite a idade de dois homens \n");
@@ -35,12 +24,9 @@ int main()
 	printf("Digite a idade de duas mulheres \n");
 	scanf("%d %d", &mulher[0], &mulher[1]);
 	
-
-	maior(homem[0], homem[1]);
 	results[0] = maior(homem[0], homem[1]) + menor(mulher[0], mulher[1]);
 	results[1] = menor(homem[0], homem[1]) * maior(mulher[0], mulher[1]);
 
 	printf("%.2f \n", results[0]);
 	printf("%.2f \n", results[1]);
-
 }
